99ae4e46b98e8ce97f26f7b8df1601e2d315ed82
Paweso <paweso@wp.pl>
2020-08-25 07:05:21 +0200

    On branch master

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-
+        String hotelName = "Overlook";
+        int systemVersion = 1;
+        boolean isDeveloperVersion = true;
+        System.out.print("Witam w systemie rezerwacji dla hotelu");
+        System.out.println(hotelName);
+        System.out.println("Aktualna wersja systemu: ");
+        System.out.println(systemVersion);
+        System.out.println("Wersja developerska: ");
+        System.out.println(isDeveloperVersion);
+        System.out.println("\n=========================\n");
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. Dodaj nowego gościa.");
+        System.out.println("2. Dodaj nowy pokój.");
+        System.out.println("3. Wyszukaj gościa.");
+        System.out.println("Wybierz opcję: ");
+        int option = 0;
+        try {
+            option = input.nextInt();
+        } catch (Exception e) {
+            System.out.println("Niepoprawne dane wejsciowe, wprowadz liczbę.");
+            e.printStackTrace();
+        }
+        if (option == 1) {
+            System.out.println("Wybrano opcję 1.");
+        } else if (option == 2) {
+            System.out.println("Wybrano opcję 2.");
+        } else if (option == 3) {
+            System.out.println("Wybrano opcję 3.");
+        } else {
+            System.out.println("Wybrano niepoprawną akcję.");
+        }
     }
 }
